eef26646a3741d73dd941f9f481fab3844df8705
Malkhaz Khubulovi <mxubulovi1998@gmail.com>
2024-04-10 12:24:40 +0400

    learning RxJava and practik on it

--- app/src/main/java/com/example/myapplication/presentation/conventer/ConverterScreen.kt
+++ app/src/main/java/com/example/myapplication/presentation/conventer/ConverterScreen.kt
@@ -3,8 +3,9 @@ package com.example.myapplication.presentation.conventer
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.example.myapplication.presentation.user.CustomFragmentScreen
+import com.github.terrakok.cicerone.Screen
 
-object ConverterScreen : CustomFragmentScreen {
+object ConverterScreen : CustomFragmentScreen,Screen {
     override fun createCustomFragment(factory: FragmentFactory): Fragment =
         ConverterFragment.newInstance()
 }
